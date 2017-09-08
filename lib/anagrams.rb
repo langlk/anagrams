48e9ab95138613ea17ed6e9a885af132c3764eea
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-08 08:53:18 -0700

    Add are_anagrams function with base case.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 
 class Anagrams
+  def self.are_anagrams(input1, input2)
+    return "These words are not anagrams."
+  end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,5 +3,8 @@
 require 'rspec'
 require 'anagrams'
 
-describe('Anagrams') do
+describe('Anagrams#are_anagrams') do
+  it('returns "These words are not anagrams." if two words are not anagrams') do
+    expect(Anagrams.are_anagrams("bob", "potato")).to(eq("These words are not anagrams."))
+  end
 end
