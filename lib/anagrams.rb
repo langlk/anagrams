567024f56fc1063c96a129dab527cde8a04991c2
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-08 08:57:05 -0700

    Update anagrams method to determine if two words with the same case are anagrams.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,6 +2,10 @@
 
 class Anagrams
   def self.are_anagrams(input1, input2)
-    return "These words are not anagrams."
+    if input1.chars.sort == input2.chars.sort
+      return "These words are anagrams."
+    else
+      return "These words are not anagrams."
+    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -7,4 +7,8 @@ describe('Anagrams#are_anagrams') do
   it('returns "These words are not anagrams." if two words are not anagrams') do
     expect(Anagrams.are_anagrams("bob", "potato")).to(eq("These words are not anagrams."))
   end
+
+  it('returns "These words are anagrams." if two words have the same letters') do
+    expect(Anagrams.are_anagrams("ruby", "bury")).to(eq("These words are anagrams."))
+  end
 end
