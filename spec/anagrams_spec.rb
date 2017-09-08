d96cbb1ccf62b0526b282927d2064c8dec7c932d
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-08 10:29:14 -0700

    Reorganize test cases.

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,6 +3,36 @@
 require 'rspec'
 require 'anagrams'
 
+describe("Anagram#is_word?") do
+  it("returns false if input does not contain a vowel (including y).") do
+    expect(Anagram.is_word?("Rrrgh")).to(eq(false))
+  end
+
+  it("returns true if input does contain a vowel (including y).") do
+    expect(Anagram.is_word?("Hello")).to(eq(true))
+  end
+end
+
+describe("Anagram#all_words?") do
+  it("returns false if any word in input does not have any vowels (including y)") do
+    expect(Anagram.all_words?("zombies like brains rrrrgh")).to(eq(false))
+  end
+
+  it("returns true if all words in input have a vowel (including y)") do
+    expect(Anagram.all_words?("zombies like brains arrrrgh")).to(eq(true))
+  end
+end
+
+describe("Anagram#are_antigrams?") do
+  it("returns false if two words have any matching letters") do
+    expect(Anagram.are_antigrams?("Cat", "Tab")).to(eq(false))
+  end
+
+  it("returns true if two words have no matching letters") do
+    expect(Anagram.are_antigrams?("Cat", "Bob")).to(eq(true))
+  end
+end
+
 describe('Anagram#are_anagrams') do
   it('returns "These words are not anagrams." if two words are not anagrams') do
     expect(Anagram.are_anagrams("bob", "potato")).to(eq("These words are not anagrams."))
@@ -40,35 +70,5 @@ describe('Anagram#are_anagrams') do
     expect(Anagram.are_anagrams("Eat desserts!", "Stressed, tea")).to(eq("These phrases are anagrams."))
     expect(Anagram.are_anagrams("This is a test!", "Word!")).to(eq("These phrases are antigrams (no letter matches)."))
     expect(Anagram.are_anagrams("A Santa!", "at Nasa...")).to(eq("These phrases are anagrams. They also form a palindrome."))
-  end
-end
-
-describe("Anagram#is_word?") do
-  it("returns false if input does not contain a vowel (including y).") do
-    expect(Anagram.is_word?("Rrrgh")).to(eq(false))
-  end
-
-  it("returns true if input does contain a vowel (including y).") do
-    expect(Anagram.is_word?("Hello")).to(eq(true))
-  end
-end
-
-describe("Anagram#are_antigrams?") do
-  it("returns false if two words have any matching letters") do
-    expect(Anagram.are_antigrams?("Cat", "Tab")).to(eq(false))
-  end
-
-  it("returns true if two words have no matching letters") do
-    expect(Anagram.are_antigrams?("Cat", "Bob")).to(eq(true))
-  end
-end
-
-describe("Anagram#all_words?") do
-  it("returns false if any word in input does not have any vowels (including y)") do
-    expect(Anagram.all_words?("zombies like brains rrrrgh")).to(eq(false))
-  end
-
-  it("returns true if all words in input have a vowel (including y)") do
-    expect(Anagram.all_words?("zombies like brains arrrrgh")).to(eq(true))
   end
 end
