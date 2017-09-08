c17014c151e438d008264c6115764aac10e66df3
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-08 13:55:50 -0700

    Update Dictionary.is_word? to check for word in system word file.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,6 +2,11 @@
 
 class Dictionary
   def self.is_word?(string)
+    File.readlines("/usr/share/dict/words").each do |word|
+      if word.downcase.chomp == string.downcase
+        return true
+      end
+    end
     false
   end
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,4 +7,7 @@ describe("Dictionary") do
   it("returns false if word is not in system words file") do
     expect(Dictionary.is_word?("rrrrgh")).to(eq(false))
   end
+  it("returns true if word is in system words file") do
+    expect(Dictionary.is_word?("hello")).to(eq(true))
+  end
 end
